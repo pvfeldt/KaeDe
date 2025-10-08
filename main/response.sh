81447759d04a94fd24f9a352f4d179a54f393ac6
pvfeldt <pvfeldt@yeah.net>
2025-10-09 03:35:20 +0800

    new

--- main/response.sh
+++ main/response.sh
@@ -1,6 +1,6 @@
 CUDA_VISIBLE_DEVICES=1 nohup python -u  generate_response.py \
---dataset_type CWQ \
+--dataset_type WebQSP \
 --model_name_or_path ../llm_model/llama2-7b-chat \
 --template llama2  \
---adapter_name_or_path ../checkpoint/CWQ/llama2-7b \
---num_beams 5 >>response_CWQ.log
+--adapter_name_or_path ../checkpoint/WebQSP/llama2-7b \
+--num_beams 5 >> response_WebQSP.log
--- main/search.sh
+++ main/search.sh
@@ -1,9 +1,4 @@
 CUDA_VISIBLE_DEVICES=0 nohup python search.py \
 --dataset_type WebQSP \
 --golden True \
---facc1_path /data/brr/ChatKBQA/data/common_data/facc1/ >> search_WebQSP.log
-
-CUDA_VISIBLE_DEVICES=1 nohup python search.py \
---dataset_type CWQ \
---golden True \
---facc1_path /data/brr/ChatKBQA/data/common_data/facc1/ >> search_CWQ.log
+--facc1_path ../entity_retrieval/common_data/facc1/ >> search_WebQSP.log
